ccce56cba44b127cd22d3194f49752cb54e3cf15
Kalpana-dev-fullstack <kalpanareddymittapalli@gmail.com>
2025-12-16 17:54:37 +0530

    Create database.sql

---
+++ database.sql
@@ -0,0 +1,9 @@
+CREATE TABLE students (
+    id INT PRIMARY KEY,
+    name VARCHAR(50),
+    age INT,
+    course VARCHAR(50)
+);
+
+INSERT INTO students VALUES (1, 'Kalpana', 22, 'Computer Science');
+INSERT INTO students VALUES (2, 'Ravi', 21, 'Information Technology');
