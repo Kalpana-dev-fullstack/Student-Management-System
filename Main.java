0d224270918f1c2f1a28cded252f24b6ed8fb20f
Kalpana-dev-fullstack <kalpanareddymittapalli@gmail.com>
2025-12-16 17:50:09 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Student Management System");
+
+        StudentDAO dao = new StudentDAO();
+
+        Student s1 = new Student(1, "Kalpana", 22, "Computer Science");
+        Student s2 = new Student(2, "Ravi", 21, "Information Technology");
+
+        dao.addStudent(s1);
+        dao.addStudent(s2);
+
+        System.out.println("All Students:");
+        dao.viewStudents();
+
+        dao.deleteStudent(1);
+
+        System.out.println("After Deletion:");
+        dao.viewStudents();
     }
 }
+
